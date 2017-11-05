f0851d8a1ec0a2e5d306c9631c608955328dee9d
jeanneomarie <jeanneomarie01@gmail.com>
2017-11-05 18:07:09 +0200

    ZOOWSOME PART 1

--- Students/Onofrei I. Ioana Maria/ZOOWSOME/src/javasmmr/zoowsome/controllers/MainController.java
+++ Students/Onofrei I. Ioana Maria/ZOOWSOME/src/javasmmr/zoowsome/controllers/MainController.java
@@ -1,6 +1,6 @@
 package javasmmr.zoowsome.controllers;
 
-import javasmmr.zoowsome.models.animals.Animal;
+import javasmmr.zoowsome.models.animals.*;
 import javasmmr.zoowsome.services.factories.AnimalFactory;
 import javasmmr.zoowsome.services.factories.SpeciesFactory;
 
@@ -8,15 +8,19 @@ public class MainController {
 
 	public static void main(String[] args) {
 		AnimalFactory abstractFactory = new AnimalFactory();
+		
 		SpeciesFactory speciesFactory1 = abstractFactory.getSpeciesFactory(Constants.Species.Mammals);
 		Animal a1 = speciesFactory1.getAnimal(Constants.Animals.Mammals.Tiger);
 		System.out.printf("We have an animal with %d legs!\n", a1.getNrOfLegs());
 		
+		SpeciesFactory speciesFactory2 = abstractFactory.getSpeciesFactory(Constants.Species.Insects);
+		Animal a2 = (Cockroach) speciesFactory2.getAnimal(Constants.Animals.Insects.Cockroach);
+		System.out.printf("I have a new animl made by me, wohoo! But, can it fly? ->  %s \n", ((Cockroach) a2).canFly());
 		
-		//i can'y access methods from the Insect class :(
-		//SpeciesFactory speciesFactory2 = abstractFactory.getSpeciesFactory(Constants.Species.Insects);
-		//Animal a2 = speciesFactory2.getAnimal(Constants.Animals.Insects.Cockroach);
-		//System.out.printf("I have a new animl made by me, wohoo! But, can it fly? ->  %b \n",a2.canFly());
+		SpeciesFactory speciesFactory3 = abstractFactory.getSpeciesFactory(Constants.Species.Aquatics);
+		Animal a3 = (Penguin) speciesFactory3.getAnimal(Constants.Animals.Aquatics.Penguin);
+		System.out.printf("This awesome animal who lives in %s \n", ((Penguin)a3).getWaterType() );
+		
 	
 		
 	
